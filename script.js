b5091f5dbf0cfd22e6ddc80cf2264edb8a51df7f
radhysyah <rizkia.as.ac@gmail.com>
2021-04-25 11:27:50 +0900

    adding change theme feature, add ubah tema program in script.js, adding theme button, add theme button and select option in index html, add div with background class and style attribute with none display in index html,separate appearance from style.css, fix style.css , add neonUI.css, add darkUI.css,

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navbar = document.querySelector('.navbar');
 const qa = document.querySelector('.qa');
 
 navbar.addEventListener('click', function(event) {
-	console.log(event.target);
+	// console.log(event.target);
 
 	if(event.target.className == "hammenu"){
 		qa.classList.toggle('show');
@@ -12,3 +12,26 @@ navbar.addEventListener('click', function(event) {
 
 
 // akhir bar navigasi
+
+
+// // ubah tema
+
+const link = document.querySelector('.tema');
+const theme = document.querySelector('select');
+
+const background = document.querySelector('.background');
+
+theme.addEventListener('change', function(event) {
+	// console.log(event.target.value);
+	if(event.target.value == 'Neon') {
+		link.href = 'neonUI.css';
+		background.style.display = 'block';
+	}
+	if(event.target.value == 'Dark') {
+		link.href = 'darkUI.css';
+		background.style.display = 'none';
+	}
+});
+
+
+// // akhir ubah tema
